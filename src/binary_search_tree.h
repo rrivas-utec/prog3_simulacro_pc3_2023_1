e3244f3c4f832b47f713ce595cd1cbe6d163ac9d
rivasr <rivasr@gitlab.buildone.co>
2023-07-13 17:55:04 -0500

    update

--- src/binary_search_tree.h
+++ src/binary_search_tree.h
@@ -5,6 +5,132 @@
 #ifndef PROG3_SIMULACRO_PC3_2023_1_BINARY_SEARCH_TREE_H
 #define PROG3_SIMULACRO_PC3_2023_1_BINARY_SEARCH_TREE_H
 
+#include <memory>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+using namespace std;
 
+template <
+        typename KeyType,
+        typename ValueType,
+        typename Compare=less<KeyType>
+>
+class binary_search_tree {
+    // Sección de tipos
+    struct node_t; // Declaración adelantada o forward
+    using item_t = pair<KeyType, ValueType>;
+    using shared_ptr_t = shared_ptr<node_t>;
+    using weak_ptr_t = weak_ptr<node_t>;
+    struct node_t {
+        item_t item{};
+        shared_ptr_t left;
+        shared_ptr_t right;
+        weak_ptr_t parent;
+        node_t(const item_t& item, shared_ptr_t parent)
+                : item(item), parent(parent) {}
+    };
+    shared_ptr_t root;
+    template<typename Callable>
+    void _in_order(shared_ptr_t current, const Callable& fun) {
+        // 1. Si árbol es vacío terminar
+        if (!current) return;
+
+        // 2. Ir al subárbol izquierda
+        _in_order(current->left, fun);
+        // 3. Obtener el nodo
+        fun(current->item);
+        // 4. Ir al subárbol derecho
+        _in_order(current->right, fun);
+    }
+public:
+    binary_search_tree() = default;
+    void insert(const item_t& item) {
+        // Verificar si tiene root
+        if (!root) { // Si no hay root
+            root = make_shared<node_t>(item, nullptr);
+            return;
+        }
+        // Creo el concurrente
+        auto current = root;
+        while (current) {
+            if (item.first < current->item.first) {
+                // Colocar a la izquierda
+                if (!current->left) { // Si no existe hijo de izquierda
+                    current->left = make_shared<node_t>(item, current);
+                    break;
+                }
+                else {
+                    current = current->left;
+                }
+            }
+            else {
+                // Colocar a la derecha
+                if (!current->right) { // Si no existe hijo de izquierda
+                    current->right = make_shared<node_t>(item, current);
+                    break;
+                }
+                else {
+                    current = current->right;
+                }
+            }
+        }
+    }
+    template<typename Callable>
+    void bfs(const Callable& fun) {
+        // Definir un queue
+        queue<shared_ptr_t> q;
+        // 1. Agregar (push) del nodo raíz al queue
+        q.push(root);
+        // 2. Mientras el queue no este vacía (empty)
+        while (!q.empty()) {
+            // 3. Obtener (front) el primer valor del queue
+            auto current = q.front();
+            fun(current->item);
+            // 4. Retirar(pop) el primer valor del queue
+            q.pop();
+            // 5. Agregar (push) todos los nodos hijos de nodo extraído hacia el queue.
+            if (current->left) q.push(current->left);
+            if (current->right) q.push(current->right);
+        }
+    }
+    template<typename Callable>
+    void dfs(const Callable& fun) {
+        // Definir el queue y el unordered_set
+        stack<shared_ptr_t> s;
+        unordered_set<shared_ptr_t> no_available;
+        auto is_available = [&no_available](auto node) {
+            return no_available.find(node) == no_available.end(); };
+        // 1. Agregar (push) el nodo raíz al stack
+        s.push(root);
+        // 2. Mientras stack no este vacío (empty)
+        while (!s.empty()) {
+            // 3. Obtener (top) nodo del stack y hacer no disponible
+            auto current = s.top();
+            if (is_available(current)) {
+                fun(current->item);
+            }
+            no_available.insert(current);
+            // 4. Si tiene hijos disponibles:
+            //      Agregar (push) el primer nodo hijo disponible al stack.
+            if (current->left && is_available(current->left)) {
+                s.push(current->left);
+            }
+            else if (current->right && is_available(current->right)) {
+                s.push(current->right);
+            }
+                // 5. Sino:
+            else {
+                //      Retirar (pop) nodo del stack
+                s.pop();
+            }
+        }
+    }
+    template<class Callable>
+    void in_order(const Callable& fun) {
+        _in_order(root, fun);
+    }
+};
 
 #endif //PROG3_SIMULACRO_PC3_2023_1_BINARY_SEARCH_TREE_H
--- src/grafo.h
+++ src/grafo.h
@@ -5,6 +5,142 @@
 #ifndef PROG3_SIMULACRO_PC3_2023_1_GRAFO_H
 #define PROG3_SIMULACRO_PC3_2023_1_GRAFO_H
 
+#include <utility>
+#include <unordered_map>
+#include <vector>
+#include <forward_list>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <algorithm>
+#include <stdexcept>
 
+using namespace std;
+
+template<typename KeyType, typename ValueType, typename WeightType>
+class grafo {
+    using VertexType = size_t;
+    using AdjacentType = pair<VertexType, WeightType>;
+    using ItemType = pair<VertexType, ValueType>;
+    using EdgeType = tuple<VertexType, VertexType, WeightType>;
+    // Lista de Vertices
+    unordered_map<KeyType, ItemType> vertices;
+    // Vertices Adyacentes
+    vector<forward_list<AdjacentType>> adjacent_vertices;
+    KeyType get_key(VertexType vertex) {
+        auto it = find_if(begin(vertices), end(vertices), [vertex](const auto& item){
+            return item.second.first == vertex;
+        });
+        if (it == end(vertices))
+            throw runtime_error("key no encontrada");
+        return it->first;
+    }
+public:
+    grafo() = default;
+    bool add_vertex(pair<KeyType, ValueType> node) {
+        // Buscar el vertex
+        if (vertices.find(node.first) == end(vertices)) {
+            const auto [key, value] = node;
+            vertices.try_emplace(key, pair{size(adjacent_vertices), value});
+            adjacent_vertices.emplace_back();
+            return true;
+        }
+        return false;
+    }
+    void add_edge(KeyType key_vertex1, KeyType key_vertex2, WeightType weight) {
+        auto vx1 = vertices[key_vertex1].first;
+        auto vx2 = vertices[key_vertex2].first;
+        adjacent_vertices[vx1].emplace_front(vx2, weight);
+        adjacent_vertices[vx2].emplace_front(vx1, weight);
+    }
+    template<typename UnaryFunction>
+    void bfs (KeyType key, UnaryFunction fun) {
+        queue<VertexType> q;
+        unordered_set<VertexType> visited;
+        auto is_visited= [&visited](VertexType vertex) {
+            return visited.find(vertex) != end(visited);
+        };
+        // Paso #1
+        auto fv = vertices[key].first;
+        q.push(fv);
+        visited.insert(fv);
+        // Paso #2
+        bool exit = false;
+        while(!q.empty()) {
+            // Paso #3
+            auto vx = q.front();
+            fun(get_key(vx), exit);
+            if (exit) break;
+            // Paso #4
+            q.pop();
+            // Paso #5
+            for (auto [va, w]: adjacent_vertices[vx]) {
+                if (!is_visited(va)) {
+                    q.push(va);
+                    visited.insert(va);
+                }
+            }
+        }
+    }
+    template<typename UnaryFunction>
+    void dfs (KeyType key, UnaryFunction fun) {
+        stack<VertexType> s;
+        unordered_set<VertexType> visited;
+        // Paso #1
+        auto fv = vertices[key].first;
+        s.push(fv);
+        visited.insert(fv);
+        // Paso #2
+        bool exit = false;
+        while(!s.empty()) {
+            // Paso #3
+            auto vx_top = s.top();
+            // Paso # 4.0 Buscar el primer adyacente no visitado
+            const auto& adj = adjacent_vertices[vx_top];
+            auto it = find_if(begin(adj), end(adj), [&visited](auto item) {
+                return visited.find(item.first) == end(visited);
+            });
+            // Paso #4
+            if (it != end(adj)) {
+                s.push(it->first);
+                visited.insert(it->first);
+            }
+                // Paso #5
+            else {
+                fun(get_key(vx_top));
+                s.pop();
+            }
+        }
+    }
+    template<typename UnaryFunction>
+    void prim(KeyType key, UnaryFunction fun) {
+        auto edge_compare = [](auto e1, auto e2) {
+            return get<2>(e1) > get<2>(e2);
+        };
+        priority_queue<EdgeType, vector<EdgeType>, decltype(edge_compare)> pq;
+        unordered_set<VertexType> visited;
+        auto is_visited= [&visited](VertexType vertex) {
+            return visited.find(vertex) != end(visited);
+        };
+        // Agregar al pq las arista adyacentes al vertex de key
+        auto fvx = vertices[key].first;
+        for (auto [fva, fw]: adjacent_vertices[fvx]) {
+            pq.push({fvx, fva, fw});
+        }
+        visited.insert(fvx);
+        // Bucle principal
+        while (!pq.empty()) {
+            auto [vx, va, w] = pq.top();
+            pq.pop();
+            if (!is_visited(va)) {
+                fun(get_key(vx), get_key(va), w);
+                visited.insert(va);
+                for(auto [vaa, waa]: adjacent_vertices[va]) {
+                    pq.push({va, vaa, waa});
+                }
+            }
+        }
+    }
+};
 
 #endif //PROG3_SIMULACRO_PC3_2023_1_GRAFO_H
